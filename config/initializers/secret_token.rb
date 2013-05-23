e301dfe4e5bc8e54f3ba0d222189fbc64c246dd3
Karlo Soriano <dev+karlo@aelogica.com>
2013-05-23 14:07:26 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeaveTracker::Application.config.secret_key_base = '68e63eaa02caff775bfc973ca359a8a4c4f788d6602a758c9cc73c141535dc15d54e9f1942dd3d2f04cb91b9b225561a411340dd28a4c5ce479725910ca34313'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LeaveTracker::Application.config.session_store :cookie_store, key: '_leave_tracker_session'
