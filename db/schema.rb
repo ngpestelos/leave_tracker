e0e5a9037ea34dfd4a1d9cfe193d22b5a43604d2
Karlo Soriano <dev+karlo@aelogica.com>
2013-05-29 10:44:01 +0800

    added role field for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130528002755) do
+ActiveRecord::Schema.define(version: 20130529023554) do
 
   create_table "leave_types", force: true do |t|
     t.string   "description"
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 20130528002755) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",         null: false
+    t.string   "encrypted_password",     default: "",         null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20130528002755) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role",                   default: "employee"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
