f1a6a231b6d26905767b5ee3dec37f40a51d1a69
Karlo Soriano <dev+karlo@aelogica.com>
2013-05-30 12:58:18 +0800

    added check for nil leave requests

--- app/models/leave.rb
+++ app/models/leave.rb
@@ -1,22 +1,23 @@
 class Leave < ActiveRecord::Base
-  validates :date, :presence => true 
-  validates :leave_type, :presence => true
+  validates :date, :leave_type_id, :presence => true 
   belongs_to :user, :class_name => "User"
   belongs_to :leave_type, :class_name => "LeaveType"
 
-  before_validation :check_remaining_leaves
+  after_validation :check_remaining_leaves
 
   private
 
   def check_remaining_leaves
-    user = User.find(self.user_id)
-    if self.id.nil?
-      current_count_by_leave_type = user.leaves.where(:leave_type_id => self.leave_type_id).count
-      max_count_by_leave_type = user.number_of_leaves_per_types.find_by_leave_type_id(self.leave_type_id).number
-      if current_count_by_leave_type == max_count_by_leave_type
-        return false
-      else
-        return true
+    if self.errors.blank?
+      user = User.find(self.user_id)
+      if self.id.nil?
+        current_count_by_leave_type = user.leaves.where(:leave_type_id => self.leave_type_id).count
+        max_count_by_leave_type = user.number_of_leaves_per_types.find_by_leave_type_id(self.leave_type_id).number
+        if current_count_by_leave_type == max_count_by_leave_type
+          return false
+        else
+          return true
+        end
       end
     end
   end
